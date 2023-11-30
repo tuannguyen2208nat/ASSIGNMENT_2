2f167aeee5d88480c8bb6de8a41c8e8d573da58d
DESKTOP-QUEAO1U\tuann <tuan.nguyen2208nat@hcmut.edu.vn>
2023-11-30 09:00:00 +0700

    change single linklist -> double linklist

--- restaurant.cpp
+++ restaurant.cpp
@@ -7,90 +7,195 @@ public:
 	char data;
 	int freq;
 	node *next;
-	node() : data('\0'), freq(0), next(nullptr){};
-	node(char data) : data(data), freq(1), next(nullptr){};
+	node *prev;
+	node() : data('\0'), freq(0), next(nullptr), prev(nullptr){};
+	node(char data, node *temp) : data(data), freq(1), next(nullptr), prev(temp){};
+	node(char data) : data(data), freq(1), next(nullptr), prev(nullptr){};
 };
-node *addElement(node *head, node *temp, char data)
+class Linklist
 {
-	node *p = head;
-	while (p != nullptr)
+public:
+	node *head;
+	node *tail;
+	/////////////////
+	void Linklist_print()
 	{
-		if (p->data == data)
+		node *temp = head;
+		while (temp != nullptr)
 		{
-			p->freq++;
-			return temp;
+			cout << temp->data << " " << temp->freq << endl;
+			temp = temp->next;
 		}
-		p = p->next;
 	}
-	node *l = new node(data);
-	temp->next = l;
-	return l;
-}
-void print(node *temp)
+	/////////////////
+	int Linklist_size()
+	{
+		int count = 0;
+		node *temp = head;
+		while (temp != nullptr)
+		{
+			count++;
+			temp = temp->next;
+		}
+		return count;
+	}
+	/////////////////
+	node *Linklist_gethead()
+	{
+		return head;
+	}
+	/////////////////
+	void LAPSE_caesar()
+	{
+		node *temp = head;
+		while (temp != nullptr)
+		{
+			if (isalpha(temp->data))
+			{
+				char base = isupper(temp->data) ? 'A' : 'a';
+				temp->data = (temp->data - base + temp->freq) % 26 + base;
+			}
+			temp = temp->next;
+		}
+	}
+	/////////////////
+	node *search_node(char data)
+	{
+		node *current = head;
+		while (current != nullptr)
+		{
+			if (current->data == data)
+			{
+				break;
+			}
+			current = current->next;
+		}
+		return current;
+	}
+	void delete_samenode()
+	{
+		set<char> seenData;
+		node *current = head;
+
+		while (current != nullptr)
+		{
+			if (seenData.count(current->data) > 0)
+			{
+				node *p = search_node(current->data);
+				int n = current->freq;
+				p->freq += n;
+				node *temp = current;
+				current = current->next;
+
+				if (temp->prev != nullptr)
+				{
+					temp->prev->next = temp->next;
+				}
+				else
+				{
+					head = temp->next;
+				}
+
+				if (temp->next != nullptr)
+				{
+					temp->next->prev = temp->prev;
+				}
+				else
+				{
+					tail = temp->prev;
+				}
+
+				delete temp;
+			}
+			else
+			{
+				seenData.insert(current->data);
+				current = current->next;
+			}
+		}
+	}
+	/////////////////
+};
+
+Linklist *newlist(char data)
 {
+	Linklist *arr = new Linklist;
+	arr->head = new node(data);
+	arr->tail = arr->head;
+	return arr;
+}
+Linklist *addElement(Linklist *arr, char data)
+{
+	node *temp = arr->head;
 	while (temp != nullptr)
 	{
-		cout << temp->data << " " << temp->freq << endl;
-		temp = temp->next;
-	}
-}
-void LAPSE_sort(node **head)
-{
-	node *prev = (*head);
-	node *curr = (*head)->next;
-
-	// Traverse list
-	while (curr != NULL)
-	{
-		// If curr is smaller than prev,
-		// then it must be moved to head
-		if (curr->freq > prev->freq)
+		if (temp->data == data)
 		{
-			prev->next = curr->next;
-			curr->next = (*head);
-			(*head) = curr;
-			curr = prev;
-		}
-		else
-		{
-			prev = curr;
-		}
-
-		// Move current
-		curr = curr->next;
-	}
-}
-void LAPSE_caesar(node *temp)
-{
-	while (temp != nullptr)
-	{
-		if (isalpha(temp->data))
-		{
-			char base = isupper(temp->data) ? 'A' : 'a';
-			temp->data = (temp->data - base + temp->freq) % 26 + base;
+			temp->freq++;
+			return arr;
 		}
 		temp = temp->next;
 	}
+	node *p = new node(data, arr->tail);
+	arr->tail->next = p;
+	arr->tail = p;
+	return arr;
 }
-void deleteNode(node *p)
+/////////////////
+void swap(node *a, node *b)
 {
+	char temp_data = a->data;
+	int temp_freq = a->freq;
+	a->data = b->data;
+	a->freq = b->freq;
+	b->data = temp_data;
+	b->freq = temp_freq;
 }
-void LAPSE_congdon(node *temp)
+void LAPSE_sort(Linklist &list)
 {
+	if (list.head == nullptr || list.head->next == nullptr)
+	{
+		return;
+	}
+
+	bool swapped;
+	node *ptr1;
+	node *lptr = nullptr;
+	do
+	{
+		swapped = false;
+		ptr1 = list.head;
+
+		while (ptr1->next != lptr)
+		{
+			if (ptr1->freq > ptr1->next->freq || (ptr1->freq == ptr1->next->freq && int(ptr1->data) > int(ptr1->next->data)))
+			{
+				swap(ptr1, ptr1->next);
+				swapped = true;
+			}
+			ptr1 = ptr1->next;
+		}
+		lptr = ptr1;
+	} while (swapped);
 }
+/////////////////
 void LAPSE_main(string name)
 {
-	node *head = new node(name[0]);
-	node *temp = head;
+	if ((int)name.size() <= 0)
+	{
+		return;
+	}
+	Linklist *arr = newlist(name[0]);
 	for (int i = 1; i < (int)name.size(); i++)
 	{
-		temp = addElement(head, temp, name[i]);
+		arr = addElement(arr, name[i]);
 	}
-	print(head);
-	cout << endl;
-	LAPSE_caesar(head);
-	LAPSE_congdon(head);
-	print(head);
+	arr->LAPSE_caesar();
+	arr->delete_samenode();
+
+	node *head = arr->Linklist_gethead();
+	LAPSE_sort(*arr);
+	arr->Linklist_print();
 }
 
 void LAPSE(string name)
